6aaa82833a47d62d54a06d97c5f48f44635c8894
myselfanandvp <mailanandvp@gmail.com>
2026-01-03 03:16:04 +0530

    updated the add button

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -12,7 +12,7 @@ function TodoForm({ addTodo }) {
 
   return (
     <form onSubmit={handleSubmit} className="mb-8 relative group">
-      <div className="absolute -inset-0.5 bg-gradient-to-r from-cat-pink to-cat-mauve rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
+      <div className="absolute -inset-0.5 bg-linear-to-r from-cat-pink to-cat-mauve rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
       <div className="relative flex items-center bg-cat-surface0 rounded-lg leading-none">
         <input
           type="text"
@@ -23,9 +23,11 @@ function TodoForm({ addTodo }) {
         />
         <button
           type="submit"
-          className="absolute right-2 px-4 py-2 bg-cat-mauve text-cat-base font-bold rounded-md hover:bg-cat-pink transition-colors pointer-events-auto"
+          className="absolute right-2 px-4 py-2 bg-cat-mauve text-cat-base font-bold rounded-md hover:bg-cat-pink  pointer-events-auto transition duration-150 ease-in-out transform hover:scale-105 active:scale-95 shadow-xl/30
+ active:shadow-sm  md:px-6 md:py-5  h-5 w-34  uppercase
+ m-2 flex items-center justify-center gap-2 text-shadow-lg/30"
         >
-          Add
+          Add task
         </button>
       </div>
     </form>
